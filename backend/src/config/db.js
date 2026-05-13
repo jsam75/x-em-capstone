54e7e28f1a72a2f49db56ced783be50c810b2223
jsam75 <phoenix1201@gmail.com>
2026-05-13 10:45:59 -0500

    More production debugging

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,4 +9,20 @@ export const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+ 
 });
+
+// Production Debugging 
+console.log("DB CONFIG:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+
+db.getConnection()
+  .then(() => console.log("DB CONNECTION SUCCESS"))
+  .catch(err => {
+    console.error("DB CONNECTION FAILURE");
+    console.error(err);
+  });
