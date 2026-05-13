b3252a0bfbf44bd82f7f658365019bcf67a043c4
jsam75 <phoenix1201@gmail.com>
2026-05-13 10:53:58 -0500

    Production debugging in error handler

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,11 +1,21 @@
 export const errorHandler = (err, req, res, next) => {
   //console.error("Server Error:", err.message);
+
+  // Production Debugging Logs
   console.error("===== GLOBAL ERROR =====");
   console.error(err);
   console.error("TYPE:", typeof err);
   console.error("MESSAGE:", err?.message);
   console.error("STACK:", err?.stack);
   console.error("========================");
+
+  console.error("FULL ERROR OBJECT:", JSON.stringify(err, null, 2));
+  if (err?.errors) {
+  console.error("AGGREGATE INNER ERRORS:");
+  err.errors.forEach((e, index) => {
+    console.error(`INNER ERROR ${index}:`, e);
+  });
+}
 
   if (err?.code === "ER_DUP_ENTRY") {
     return res.status(400).json({
@@ -23,6 +33,8 @@ export const errorHandler = (err, req, res, next) => {
 
   res.status(500).json({
     success: false,
-    message: err?.message || "Unknown server error"
+    message: err?.message || "Unknown server error",
+    stack: err?.stack,
+    errors: err?.errors || null
   });
 };
